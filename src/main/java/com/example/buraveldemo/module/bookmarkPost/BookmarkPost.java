267faa45b0c41dd1c3f57845cb1d70d68d6b2c12
skarltjr <kisa0828@naver.com>
2021-04-03 13:47:45 +0900

    planTag구성

--- src/main/java/com/example/buraveldemo/module/bookmarkPost/BookmarkPost.java
+++ src/main/java/com/example/buraveldemo/module/bookmarkPost/BookmarkPost.java
@@ -29,5 +29,9 @@ public class BookmarkPost {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
+
+    private boolean checked;
+    // 나의 여행으로 가져갈 때 채크하면 check == true
+    //이 후  bookmarkPostManager가 !나! 이면서 checked == true인 bookmarkPost들을 가져오고 드래그로 끌어다 plan에 추가 등
 }
 
